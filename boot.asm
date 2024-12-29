1781fd5574d412b9c31a42a494949371131aa4a7
Mikail <mikailsyed@yahoo.com>
2024-12-28 23:21:55 -0500

    More bootloader stuff lol

--- boot.asm
+++ boot.asm
@@ -17,12 +17,12 @@ times 33 db 0
 crossover:
     jmp 0x7C0:start_program
 
-interr_zero:
-    mov ah, 0eh
-    mov al, 'L'
-    mov bx, 0x00
-    int 0x10
-    iret
+; interr_zero: ; IVT proof of concept
+;     mov ah, 0eh
+;     mov al, 'L'
+;     mov bx, 0x00
+;     int 0x10
+;     iret
 
 start_program:
     cli
@@ -38,23 +38,34 @@ start_program:
     sti
 
     ; READING FROM THE HARD DISK via CHS
-    ; mov ah, 2 ; This reads from the sector command
-    ; mov al, 1 ; Chooses one sector to read from
-    ; mov ch, 0 ; Cylinder low of 8 bits
-    ; mov cl, 2 ; Read of the sector 2
-    ; mov dh, 0 ; CHS Head number/mark
+    mov ah, 2 ; This reads from the sector command
+    mov al, 1 ; Chooses one sector to read from
+    mov ch, 0 ; Cylinder low of 8 bits
+    mov cl, 2 ; Read of the sector 2
+    mov dh, 0 ; CHS Head number/mark
 
+    mov bx, buffer_dest
 
+    int 0x13 ; Interrupt for hard disk
 
-    mov word[ss:0x00], interr_zero
-    mov word[ss:0x02], 0x7C0
+    ;mov word[ss:0x00], interr_zero ; IVT proof of concept
+    ;mov word[ss:0x02], 0x7C0
 
-    int 0 ; you can also store 0x00 in a register and divide by zero to call the exception
+    ;int 0 ; you can also store 0x00 in a register and divide by zero to call the exception
     ; and summon the aforementioned function
 
-    mov si, msg
-    call print_whole
+    jc error_occur
 
+    mov si, buffer_dest
+    call print_whole ; Assuming this buffer works and allocates space correctly, the test.txt from the
+    ; hard disk should load
+
+    jmp $
+
+
+error_occur:
+    mov si, error_msg
+    call print_whole
     jmp $ ; keep looping in the same current area, until all assembling for bytes is done, then we can finally execute the dw command
 
 print_whole: ; prints whole string given in msg
@@ -72,6 +83,9 @@ print_individual:
     int 0x10
     ret
 
+error_msg:
+    db 'Failed to load sector', 0
+
 msg:
     db 'McKyle OS', 0
 
@@ -79,3 +93,4 @@ times 510-($ - $$) db 0 ; run 510 times, initializing in the db register 1 byte 
 ; therefore ($ - $$) is the difference between the current byte and the beginning byte */
 dw 0xAA55 ; as x86 is little endian in nature, this will actually be read as 55AA ; represents execution of the code 
 
+buffer_dest:
